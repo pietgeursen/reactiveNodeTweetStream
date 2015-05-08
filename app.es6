1adace1c18bb2c3d3e3fd7baaf9b578c02c8cc64
Piet Geursen <pietgeursen@gmail.com>
2015-05-08 15:42:24 +1200

    use rx to start mapping tweet streams. Sexy

--- app.es6
+++ app.es6
@@ -1,5 +1,40 @@
 let Twit = require('twit')
 let config = require('config');
+let Rx = require('rx');
+
 let twit_config = config.get('twitter');
 
 let T = new Twit(twit_config);
+
+let stream = T.stream('statuses/filter', {track: 'lol'});
+
+
+let tweets = Rx.Observable.fromEvent(stream, 'tweet');
+let simpleTweet = tweets.map(tweet => {
+
+	return {
+		text: tweet.text,
+		coordinates: tweet.coordinates,
+		user: {
+			location: tweet.user.location,
+			followers_count: tweet.user.followers_count
+		}
+	}
+})
+
+let subscription = simpleTweet.subscribe(
+    function (x) {
+        console.log(x);
+    },
+    function (err) {
+        console.log('Error: ' + err);
+    },
+    function () {
+        console.log('Completed');
+    });
+
+
+// T.get('search/tweets', { q: 'banana since:2011-11-11', count: 100 }, function(err, data, response) {
+//   console.log(data)
+// })
+
--- app.js
+++ app.js
@@ -2,6 +2,35 @@
 
 var Twit = require('twit');
 var config = require('config');
+var Rx = require('rx');
+
 var twit_config = config.get('twitter');
 
 var T = new Twit(twit_config);
+
+var stream = T.stream('statuses/filter', { track: 'lol' });
+
+var tweets = Rx.Observable.fromEvent(stream, 'tweet');
+var simpleTweet = tweets.map(function (tweet) {
+
+    return {
+        text: tweet.text,
+        coordinates: tweet.coordinates,
+        user: {
+            location: tweet.user.location,
+            followers_count: tweet.user.followers_count
+        }
+    };
+});
+
+var subscription = simpleTweet.subscribe(function (x) {
+    console.log(x);
+}, function (err) {
+    console.log('Error: ' + err);
+}, function () {
+    console.log('Completed');
+});
+
+// T.get('search/tweets', { q: 'banana since:2011-11-11', count: 100 }, function(err, data, response) {
+//   console.log(data)
+// })
